a7851739f0c53bf425d718b69ac27648e5fa869b
dataKernel <lancelot.saintleger@gmail.com>
2026-01-14 15:42:00 +0000

    testing create_table func to add array of cols before converting to tuples with the table name

--- crud.py
+++ crud.py
@@ -1,13 +1,23 @@
 def     create_table():
-    pass
-
+    colTableArray = []
+    
+    test = ()
+    tableName = input("What is your table name? :")
+    colNbr = int(input("How many columns you need?: "))
+    print("Registering the columns in your table now...")
+    for i in range(colNbr):
+        elem = input(f"column[{i}]:")
+        colTableArray.append(elem)
+        
+    print("---debug---")
+    print(f"tableName: {tableName}")
+    print(f"colTableArray: {colTableArray}")
+    
+        
 def     main():
     print("entry pgm...")
     
-    tableStructArray = [
-        [("WorkerTable", ["id", "name", "firstName", "age", "function"])],
-        [("AnimalTable", ["id", "type", "color", "weight"])]
-    ]   
+    create_table()
 
 if(__name__ == "__main__"):
     main()
