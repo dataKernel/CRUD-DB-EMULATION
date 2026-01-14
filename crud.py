65f725d4dc479698568aa63bc02e7cc349305968
dataKernel <lancelot.saintleger@gmail.com>
2026-01-14 15:21:32 +0000

    update file name, and add list of tupples for testing data structures (tables)

---
+++ crud.py
@@ -0,0 +1,13 @@
+def     create_table():
+    pass
+
+def     main():
+    print("entry pgm...")
+    
+    tableStructArray = [
+        [("WorkerTable", ["id", "name", "firstName", "age", "function"])],
+        [("AnimalTable", ["id", "type", "color", "weight"])]
+    ]   
+
+if(__name__ == "__main__"):
+    main()
