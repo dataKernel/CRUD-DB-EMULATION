6e0b360497c769f4284ce3b2b4159f4e9bf1f971
dataKernel <lancelot.saintleger@gmail.com>
2026-01-29 18:41:42 +0100

    update the struct and testing elements

--- crud.py
+++ crud.py
@@ -18,33 +18,50 @@ def     create_table()-> list:
     return colTableArray
         
         
-def     main():
-    
-    
+def     main():    
     #create_table()
     #restructuring table with dicos
-    tableArray = 
-    {
-        "classTable":
+    tableArray = {
+        'classTable':
         {
             'struct': ["id", "name", "active"],
             'data': [
-                {'id': 0, 'name': "rogue", 'active': True},
-                {'id': 1, 'name': "warrior", 'active': False}
+                {
+                    'id': 0, 
+                    'name': "rogue", 
+                    'active': True
+                },
+                {
+                    'id': 1, 
+                    'name': "warrior", 
+                    'active': False
+                }
             ]
         },
-        "animalTable": 
+        'animalTable': 
         {
-            'id': 0,
-            'name': "medor",
-            'type': "dog",
-            'color': "black"
+            'struct': ["id", "type", "color", "playful"],
+            'data': [
+                {
+                    'id': 0, 
+                    'type': "cat", 
+                    'color': "white", 
+                    'playful': True
+                }
+            ]
         }
     }
     
-    print(f"tableArray: {tableArray}")
-    #tableArray = create_table()
-    #print(f"exemple natif: {tableArray}")
-    
+    print(f"tableArray:{tableArray}")
+    print(f"classTable:{tableArray['classTable']}")
+    for tableName in tableArray:
+        print(f"tableName:{tableName}")
+    for tableName in tableArray:
+        print(f"elems from struct: {tableName}")
+        print("-------------------------------")
+        for elem in tableArray[tableName]['struct']:
+            print(f"elem:{elem}")
+            
+            
 if(__name__ == "__main__"):
     main()
